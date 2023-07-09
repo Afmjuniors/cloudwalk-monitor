da6185664382ccc90d1fe9a6d9938aaedce61f5a
Afmjuniors <afmjuniors@gmail.com>
2023-07-09 19:40:01 -0300

    feat: endpoitn get

--- app.py
+++ app.py
@@ -5,17 +5,25 @@ from business.transaction_business import TransactionBusiness
 
 app = Flask(__name__)
 
-transaction = TransactionBusiness()
+transaction_business = TransactionBusiness()
 
-
+# comentar
 @app.route('/api/transactions', methods=['POST'])
 def receive_transaction():
     csv_file = request.files['file']  # Obtém o arquivo CSV do corpo da solicitação
     df = pd.read_csv(csv_file)
 
-    recommendation = transaction.transaction_business(df)
+    recommendation = transaction_business.transaction_business(df)
 
     return 'Sent transaction with sucess'
+
+
+@app.route('/api/transactions', methods=['GET'])
+def receive_transaction():
+    output = None
+    # output = transaction.transaction_business(df)
+
+    return output
 
 
 if __name__ == '__main__':
