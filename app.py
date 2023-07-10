224599a14e166330264fa8fcbce42cabaa61d335
Afmjuniors <afmjuniors@gmail.com>
2023-07-10 17:01:52 -0300

    refactor: small ajustments in app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
-
+from threading import Thread
 
 from controller.TransactionsController import TransactionsController
 from business.TransactionsBusiness import TransactionBusiness
 from database.TransactionsDatabase import TransactionsDatabase
 from alert.SendEmail import SendEmail
+from monitor.realtime_monitor import create_real_time_line_graph
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
@@ -21,5 +22,15 @@ app.route('/api/transactions', methods=['POST'])(transaction_controller.receive_
 # ENDPOINT to request data for analysis, it can receive queries to indicate limit and frequency time
 app.route('/api/transactions', methods=['GET'])(transaction_controller.get_data)
 
-if __name__ == '__main__':
+
+def run_flask_server():
     app.run()
+
+
+if __name__ == '__main__':
+    # Start the Flask server in a separate thread
+    flask_thread = Thread(target=run_flask_server)
+    flask_thread.start()
+
+    # Start the real-time monitor
+    create_real_time_line_graph()
